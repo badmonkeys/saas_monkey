448ac85dad16f8c3240b6a533b9d9d6321595f71
Michael Kelly <michaelkelly322@gmail.com>
2016-07-02 05:24:28 -0400

    update puma config for rails 5

--- config/puma.rb
+++ config/puma.rb
@@ -3,4 +3,7 @@ threads Integer(ENV['WEB_MIN_THREADS'] || 1),
         Integer(ENV['WEB_MAX_THREADS'] || 16)
 
 environment ENV['RACK_ENV'] || 'development'
-port ENV['PORT'] || 3000
+port        ENV['PORT'] || 3000
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
