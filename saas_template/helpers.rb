c8abc7ef00dc6d1029015b85507fac8d0a35bb90
Michael Kelly <michaelkelly322@gmail.com>
2016-08-25 04:05:11 -0400

    adds helper file to test splitting up template file

---
+++ saas_template/helpers.rb
@@ -0,0 +1,19 @@
+$repo_url = 'https://raw.githubusercontent.com/badmonkeys/bad_monkey_rails/master'
+
+def remove_comments_for(filename)
+  gsub_file filename, /^\s*#.*\n/, ''
+end
+
+def remove_default_gemfile
+  comment_lines 'Gemfile', ''
+  remove_comments_for('Gemfile')
+end
+
+def append_to_file(filename, contents)
+  open(filename, 'a') {|f| f.puts contents }
+end
+
+def repo_get(path)
+  get "#{$repo_url}/#{path}", path
+end
+
