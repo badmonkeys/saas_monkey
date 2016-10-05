7e735161d1c9a6bf68e25aed118bd0cde09bcad8
Michael Kelly <michaelkelly322@gmail.com>
2016-10-04 20:46:03 -0400

    removes bootstrap in favor of bourbon

--- app/assets/template.rb
+++ app/assets/template.rb
@@ -3,3 +3,9 @@ run "rm app/assets/stylesheets/application.css"
 copy_file 'app/assets/stylesheets/application.scss'
 
 apply('app/assets/javascripts/template.rb')
+
+after_bundle do
+  inside './app/assets/stylesheets' do
+    run 'bundle exec bitters install'
+  end
+end
