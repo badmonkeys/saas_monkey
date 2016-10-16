89bde8524f43ad02a0c79d2780f1f062336bb7cf
Michael Kelly <michaelkelly322@gmail.com>
2016-10-16 00:55:10 -0400

    Adds heroku buildpacks, fixes heroku deploy

--- heroku.rb
+++ heroku.rb
@@ -1,23 +1,29 @@
 def heroku_name
   @heroku_name ||= app_name.dasherize
 end
-if !`which heroku`.blank?
-  run "heroku create #{heroku_name}"
-  run 'heroku plugins:install https://github.com/tpope/heroku-binstubs.git'
-  run "heroku binstubs:create #{heroku_name}"
-  run 'production addons:create heroku-postgresql:hobby-dev'
-  run 'production addons:create newrelic:wayne'
-  new_relic_key = `production config:get NEW_RELIC_LICENSE_KEY`
-  run "newrelic install --license_key='#{new_relic_key}' '#{heroku_name}'"
 
-  if apply_git?
-    run "production git:remote --app #{heroku_name}"
+after_bundle do
+  if !`which heroku`.blank?
+    run "heroku create #{heroku_name}"
+    run 'heroku plugins:install https://github.com/tpope/heroku-binstubs.git'
+    run "heroku binstubs:create #{heroku_name}"
+    run 'production addons:create heroku-postgresql:hobby-dev'
+    run 'production addons:create newrelic:wayne'
+    run 'production buildpacks:set heroku/ruby'
+    run 'production buildpacks:add https://github.com/gunpowderlabs/buildpack-ruby-rake-deploy-tasks'
+    run 'production config:set DEPLOY_TASKS=db:migrate'
+    new_relic_key = `production config:get NEW_RELIC_LICENSE_KEY`
+    run "newrelic install --license_key='#{new_relic_key}' '#{heroku_name}'"
 
-    git add: '-A .'
-    git commit: '-n -m Sets up heroku integration'
-    git push: 'origin'
+    if apply_git?
+      run "production git:remote --app #{heroku_name}"
+
+      git add: '-A .'
+      git commit: '-m "Sets up heroku integration"'
+      git push: 'heroku master'
+    end
+  else
+    fail Rails::Generators::Error, "Heroku toolbelt is not installed."\
+      "Install it from https://devcenter.heroku.com/articles/heroku-command-line"
   end
-else
-  fail Rails::Generators::Error, "Heroku toolbelt is not installed."\
-    "Install it from https://devcenter.heroku.com/articles/heroku-command-line"
 end
